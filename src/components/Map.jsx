5741d8f05a8ab16f245fc088b28c792b7cccac24
jhotadhari <tellme@waterproof-webdesign.info>
2023-07-19 18:17:41 +0200

    Install leaflet-providers and add some baselayers and a control

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,6 @@
-
-
 /**
  * External dependencies
  */
-
 // import classnames from 'classnames';
 import {
 	MapContainer,
@@ -14,13 +11,17 @@ import {
 	// useMapEvents,
 	ZoomControl,
 	// FeatureGroup,
+	// Marker,
+	// Popup,
+	// LayersControl,
 } from 'react-leaflet'
-// import L from 'leaflet';
-
+import L from 'leaflet';
+import 'leaflet-providers';
 import {
 	// useRef,
+	useContext,
 	useEffect,
-  // useState,
+  	useState,
 } from 'react';
 
 /**
@@ -29,6 +30,7 @@ import {
 import {
     defaultPosition,
 } from '../constants';
+import { ContextBaseLayers } from '../Context';
 import FullscreenControl from './FullscreenControl.jsx';
 import useStyleMaxElementHeight from '../compose/useStyleMaxElementHeight';
 
@@ -37,14 +39,71 @@ import useStyleMaxElementHeight from '../compose/useStyleMaxElementHeight';
  *
  */
 const MapInvalidateSize = ( { style } ) => {
-  const map = useMap();
-  useEffect( () => {
-      map.invalidateSize();
-  }, [style.height] );
-  return null
+	const map = useMap();
+	useEffect( () => {
+		map.invalidateSize();
+	}, [style.height] );
+	return null
 }
 
+const TileLayerProvider = ( {
+	providerVariant,
+	providerOptions,
+} ) => {
+	const {
+		layersForControl,
+		setLayersForControl,
+	} = useContext( ContextBaseLayers );
+	const map = useMap();
+	useEffect( () => {
+		if ( ! layersForControl[providerVariant] ) {
+			// Init layer, but do not add to map.
+			const layer = L.tileLayer.provider( providerVariant, providerOptions );
+			// Add new layer to layersForControl.
+			const newLayersForControl = {
+				...layersForControl,
+				[providerVariant]: layer,
+			}
+			setLayersForControl( newLayersForControl );
+		}
+	}, [layersForControl] );
+
+	useEffect( () => {
+		if ( Object.keys( layersForControl ).length ) {
+			// Set first layer active
+			const isOneLayerOnMap = Object.keys( layersForControl ).reduce( ( acc, layerKey ) => acc ? acc : !! map.hasLayer( layersForControl[layerKey] ), false );
+			if ( ! isOneLayerOnMap ) {
+				layersForControl[Object.keys( layersForControl )[0]].addTo( map );
+			}
+		}
+	}, [layersForControl] );
+
+	return null;
+};
+
+const BaseLayersControl = () => {
+	const map = useMap();
+	const { layersForControl } = useContext( ContextBaseLayers );
+	useEffect( () => {
+		let layerControl;
+		if ( Object.keys( layersForControl ).length ) {
+			// Init control and add to map
+			layerControl = L.control.layers( layersForControl );
+			layerControl.addTo( map );
+		}
+		return () => {
+			if ( layerControl ) {
+				map.removeControl( layerControl );
+			}
+		};
+	}, [layersForControl] );
+
+	return null;
+};
+
 const Map = () => {
+
+	const [layersForControl,setLayersForControl] = useState( {} );
 
     const style = useStyleMaxElementHeight();
 
@@ -52,25 +111,40 @@ const Map = () => {
         className={ 'map-wrapper' }
         style={ style }
     >
-            <MapContainer
-                center={ defaultPosition }
-                zoom={ 8 }
-                zoomSnap={ 0.1 }
-                zoomControl={ false }
-                closePopupOnClick={ false }
-            >
-                <FullscreenControl position="topright" />
+		<MapContainer
+			center={ defaultPosition }
+			zoom={ 8 }
+			zoomSnap={ 0.1 }
+			zoomControl={ false }
+			closePopupOnClick={ false }
+		>
 
-                <MapInvalidateSize style={ style } />
+			<ContextBaseLayers.Provider value={ {
+				layersForControl,
+				setLayersForControl,
+			} }>
 
-                <ZoomControl position="topright" />
+				{ [
+					'OpenStreetMap.Mapnik',
+					'Stamen.Watercolor',
+					'Esri.WorldImagery',
+					'Esri.WorldPhysical',
+				].reverse().map( layerKey => <TileLayerProvider
+					key={ layerKey }
+					providerVariant={ layerKey }
+				/> ) }
 
-                <TileLayer
-                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
+				<BaseLayersControl/>
 
-            </MapContainer>
+			</ContextBaseLayers.Provider>
+
+			<FullscreenControl position="topright" />
+
+			<MapInvalidateSize style={ style } />
+
+			<ZoomControl position="topright" />
+
+		</MapContainer>
     </div>;
 }
 
