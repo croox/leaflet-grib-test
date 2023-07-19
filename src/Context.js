0e2ee8d71d223b7e15e5383cdd59597b871e523e
jhotadhari <tellme@waterproof-webdesign.info>
2023-07-19 19:27:23 +0200

    Add static ImageLayer

--- src/Context.js
+++ src/Context.js
@@ -2,4 +2,4 @@ import {
     createContext,
 } from "react";
 
-export const ContextBaseLayers = createContext( {} );
+export const ContextLayers = createContext( {} );
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -30,7 +30,7 @@ import {
 import {
     defaultPosition,
 } from '../constants';
-import { ContextBaseLayers } from '../Context';
+import { ContextLayers } from '../Context';
 import FullscreenControl from './FullscreenControl.jsx';
 import useStyleMaxElementHeight from '../compose/useStyleMaxElementHeight';
 
@@ -47,33 +47,36 @@ const MapInvalidateSize = ( { style } ) => {
 }
 
 const TileLayerProvider = ( {
-	providerVariant,
+	providerVariant,	// required
 	providerOptions,
 } ) => {
 	const {
 		layersForControl,
 		setLayersForControl,
-	} = useContext( ContextBaseLayers );
+	} = useContext( ContextLayers );
 	const map = useMap();
 	useEffect( () => {
-		if ( ! layersForControl[providerVariant] ) {
+		if ( ! layersForControl.base[providerVariant] ) {
 			// Init layer, but do not add to map.
 			const layer = L.tileLayer.provider( providerVariant, providerOptions );
-			// Add new layer to layersForControl.
+			// Add new layer to layersForControl.base
 			const newLayersForControl = {
 				...layersForControl,
-				[providerVariant]: layer,
-			}
+				base: {
+					...layersForControl.base,
+					[providerVariant]: layer,
+				},
+			};
 			setLayersForControl( newLayersForControl );
 		}
 	}, [layersForControl] );
 
 	useEffect( () => {
-		if ( Object.keys( layersForControl ).length ) {
+		if ( Object.keys( layersForControl.base ).length ) {
 			// Set first layer active
-			const isOneLayerOnMap = Object.keys( layersForControl ).reduce( ( acc, layerKey ) => acc ? acc : !! map.hasLayer( layersForControl[layerKey] ), false );
+			const isOneLayerOnMap = Object.keys( layersForControl.base ).reduce( ( acc, layerKey ) => acc ? acc : !! map.hasLayer( layersForControl.base[layerKey] ), false );
 			if ( ! isOneLayerOnMap ) {
-				layersForControl[Object.keys( layersForControl )[0]].addTo( map );
+				layersForControl.base[Object.keys( layersForControl.base )[0]].addTo( map );
 			}
 		}
 	}, [layersForControl] );
@@ -81,14 +84,14 @@ const TileLayerProvider = ( {
 	return null;
 };
 
-const BaseLayersControl = () => {
+const LayersControl = () => {
 	const map = useMap();
-	const { layersForControl } = useContext( ContextBaseLayers );
+	const { layersForControl } = useContext( ContextLayers );
 	useEffect( () => {
 		let layerControl;
-		if ( Object.keys( layersForControl ).length ) {
+		if ( Object.keys( layersForControl.base ).length || Object.keys( layersForControl.overlay ).length ) {
 			// Init control and add to map
-			layerControl = L.control.layers( layersForControl );
+			layerControl = L.control.layers( layersForControl.base, layersForControl.overlay );
 			layerControl.addTo( map );
 		}
 		return () => {
@@ -101,9 +104,47 @@ const BaseLayersControl = () => {
 	return null;
 };
 
+const ImageLayer = ( {
+	imageUrl,		// required
+	latLngBounds,	// required
+	altText,
+	errorOverlayUrl,
+} ) => {
+	const {
+		layersForControl,
+		setLayersForControl,
+	} = useContext( ContextLayers );
+	errorOverlayUrl = errorOverlayUrl ? errorOverlayUrl : 'https://cdn-icons-png.flaticon.com/512/110/110686.png';
+
+	useEffect( () => {
+		if ( ! layersForControl.overlay[imageUrl] ) {
+			latLngBounds = Array.isArray( latLngBounds ) ? L.latLngBounds( latLngBounds ) : latLngBounds;
+			const layer = L.imageOverlay( imageUrl, latLngBounds, {
+				opacity: 0.8,
+				errorOverlayUrl: errorOverlayUrl,
+				alt: altText,
+			} );
+			// Add new layer to layersForControl.overlay.
+			const newLayersForControl = {
+				...layersForControl,
+				overlay: {
+					...layersForControl.overlay,
+					[imageUrl]: layer,
+				},
+			};
+			setLayersForControl( newLayersForControl );
+		}
+	}, [layersForControl] );
+
+	return null;
+};
+
 const Map = () => {
 
-	const [layersForControl,setLayersForControl] = useState( {} );
+	const [layersForControl,setLayersForControl] = useState( {
+		base: {},
+		overlay: {},
+	} );
 
     const style = useStyleMaxElementHeight();
 
@@ -119,7 +160,7 @@ const Map = () => {
 			closePopupOnClick={ false }
 		>
 
-			<ContextBaseLayers.Provider value={ {
+			<ContextLayers.Provider value={ {
 				layersForControl,
 				setLayersForControl,
 			} }>
@@ -134,9 +175,17 @@ const Map = () => {
 					providerVariant={ layerKey }
 				/> ) }
 
-				<BaseLayersControl/>
+				<ImageLayer
+					imageUrl={ '/static/icon-d2_germany_regular-lat-lon_single-level_2023071000_048_2d_tot_prec.png' }
+					latLngBounds={ [
+						[29.5,	-23.5],
+						[70.5, 	62.5 ],
+					] }
+				/>
 
-			</ContextBaseLayers.Provider>
+				<LayersControl/>
+
+			</ContextLayers.Provider>
 
 			<FullscreenControl position="topright" />
 
